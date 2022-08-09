30068c5bacabf475875d29af35d59b091679fd2a
Luke Bourneuf <lukebourneuf@gmail.com>
2022-08-09 14:32:40 -0400

    feat(table def) wrote table definitions for our 3 db tables

--- db/init_db.js
+++ db/init_db.js
@@ -2,14 +2,72 @@ const {
   client,
   // declare your model imports here
   // for example, User
-} = require('./');
+} = require("./");
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables...");
+    await client.query(`
+    DROP TABLE IF EXISTS cart;
+    DROP TABLE IF EXISTS orders;
+    DROP TABLE IF EXISTS products;
+    DROP TABLE IF EXISTS users;
+    
+    `);
+    console.log("Finished dropping tables");
+  } catch (error) {
+    console.error("Error dropping tables!");
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Starting to create tables");
+    await client.query(`
+        CREATE TABLE users (
+          id SERIAL PRIMARY KEY,
+          username VARCHAR(255) UNIQUE NOT NULL,
+          password VARCHAR(255) NOT NULL
+        );
+
+        CREATE TABLE products (
+          id SERIAL PRIMARY KEY,
+          name VARCHAR(255) UNIQUE NOT NULL,
+          description VARCHAR(255) NOT NULL,
+          photoURL VARCHAR(255),
+          price MONEY
+        );
+
+        CREATE TABLE orders (
+          id SERIAL PRIMARY KEY,
+          "customerId" INTEGER REFERENCES users(id) NOT NULL,
+          total MONEY NOT NULL,
+          "productId" INTEGER REFERENCES products(id) NOT NULL
+        );
+
+        CREATE TABLE cart (
+          id SERIAL PRIMARY KEY,
+          "productId" INTEGER REFERENCES products(id) NOT NULL,
+          total MONEY NOT NULL
+        );
+    
+    `);
+
+    console.log("Finished creating tables");
+  } catch (error) {
+    console.error("Error creating tables");
+    throw error;
+  }
+}
 
 async function buildTables() {
   try {
     client.connect();
 
     // drop tables in correct order
-
+    await dropTables();
+    await createTables();
     // build tables in correct order
   } catch (error) {
     throw error;
