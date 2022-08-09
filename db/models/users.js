aa60de923be1663c06a513f12c482eb664011220
MaxVillagomez <maxvillagomez5141@gmail.com>
2022-08-09 14:08:20 -0500

    feat(initial user data) created and printed initial user data onto the console

--- db/models/users.js
+++ db/models/users.js
@@ -1,6 +1,19 @@
 // grab our db client connection to use with our adapters
 const client = require("../client");
 
+async function createUser({username, password}) {
+  try {
+    const {rows: [user]} = await client.query(`
+    INSERT INTO users(username, password)
+    VALUES ($1, $2)
+    ON CONFLICT (username) DO NOTHING
+    RETURNING id, username;
+    `, [username, password])
+    return user;
+  } catch (error) {
+    throw error;
+  }
+}
 async function getAllUsers() {
   try {
     const { rows } = await client.query(`
@@ -17,5 +30,6 @@ async function getAllUsers() {
 
 module.exports = {
   // add your database adapter fns here
-  getAllUsers,
+  createUser,
+  getAllUsers
 };
